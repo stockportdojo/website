b35f5d32041ec00907c13a0c96660125329c6531
Hippyjim <hippyjim@gmail.com>
2014-08-09 17:42:14 +0100

    About CoderDojo page has content

--- webroot/about_coderdojo.php
+++ webroot/about_coderdojo.php
@@ -1,24 +1,16 @@
 <?php
 include(dirname(__FILE__) . '/../includes/header.php');
 ?>
-
 <div class="container">
-	<!-- Example row of columns -->
 	<div class="row">
 		<div class="col-md-4">
-			<h2>Heading</h2>
-			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-			<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+			<img  class="img-responsive img-thumbnail" src="/images/what_is_coderdojo.jpg" alt="Photograph of Ninjas learning scratch at Stockport Dojo" >
 		</div>
-		<div class="col-md-4">
-			<h2>Heading</h2>
-			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-			<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-		</div>
-		<div class="col-md-4">
-			<h2>Heading</h2>
-			<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-			<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+		<div class="col-md-8">
+			<h2>What is CoderDojo?</h2>
+			<p>CoderDojo is an open source, volunteer led movement orientated around running free not-for-profit coding clubs (Dojos) for young people. Since CoderDojo is an open source movement, each Dojo is different and completely autonomous! Dojos are set up, run by and led by volunteers.</p>
+			<p>At a Dojo, young people between 7 and 17 learn how to code, develop websites, apps, programs, games and much more. In addition to learning to code, members meet like minded people, show off what they’ve been working on and so on. CoderDojo makes development and learning to code a fun, sociable and awesome experience. CoderDojo also puts a strong emphasis on open source and free software, and has a strong network of members and volunteers globally. CoderDojo has just one rule: “Above All: Be Cool“, bullying, lying, wasting people’s time and so on is uncool.</p>
+			<p style="margin-top: 2em;"><a href="http://coderdojo.com/about" class="btn btn-primary btn-lg" role="button">Visit CoderDojo.com to learn more &raquo;</a></p>
 		</div>
 	</div>
 
