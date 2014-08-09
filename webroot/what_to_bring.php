9f3ff94b0e70757f4b084a5c3cc692e33598db40
Hippyjim <hippyjim@gmail.com>
2014-08-09 23:04:10 +0100

    What to bring page has some content

--- webroot/what_to_bring.php
+++ webroot/what_to_bring.php
@@ -1,36 +1,35 @@
 <?php
 include(dirname(__FILE__) . '/../includes/header.php');
 ?>
-
-<!-- Main jumbotron for a primary marketing message or call to action -->
-<div class="jumbotron">
-	<div class="container">
-		<h1>Hello, world!</h1>
-		<p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-		<p><a class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p>
-	</div>
-</div>
-
 <div class="container">
-	<!-- Example row of columns -->
 	<div class="row">
 		<div class="col-md-4">
-			<h2>Heading</h2>
-			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-			<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+			<img  class="img-responsive img-thumbnail" src="/images/what_to_bring.jpg" alt="Photograph of Ninjas learning scratch at Stockport Dojo" >
 		</div>
-		<div class="col-md-4">
-			<h2>Heading</h2>
-			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-			<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-		</div>
-		<div class="col-md-4">
-			<h2>Heading</h2>
-			<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-			<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+		<div class="col-md-8">
+			<h2>What will I need to bring?</h2>
+			<p>The only thing you really <strong>need</strong> to bring is your imagination!<p>
+
+			<p>Aside from that, a few other things will be helpful:</p>
+			<ul>
+				<li><strong>A Laptop</strong> - if you have your own laptop please bring it with you (don't forget to charge it!). If you don't have one, we can lend you one but please let us know beforehand so we can make sure we have one ready for you.</li>
+				<li><strong>An Adult</strong> - if you're under X you'll need to bring an adult with you, who will need to stay for the session (who knows, they might learn a thing or two themselves).</li>
+				<li><strong>Lunch money</strong> - there will be a lunch break from 12 – 12.30. The coffee shop will be open and serving food all day so you can purchase lunch and refreshments.</li>
+			</ul>
+			<h3>What else can I do to get ready?</h3>
+			<p>Although you don't need that much to come along and get coding, it might make things go a bit more smoothly if you can set up a couple of things beforehand. Don't worry if you get stuck, getting started is always the hardest
+				bit and the Ninjas and Mentors at the dojo will be able to get you over most hurdles.</p>
+			<p>Some software you can install:</p>
+			<ul>
+				<li><strong>Scratch</strong> - Scratch is an easy to use graphical programming language you can use to make games and animations. We use Scratch 1.4 which you can download for free from <a href="http://scratch.mit.edu/scratch_1.4/">the scratch website</a></li>
+				<li><strong>Minecraft</strong> - If you're interested in coding for Minecraft, it would be useful to <a href="https://minecraft.net/store/minecraft">buy a Minecraft account</a> beforehand. If you prefer not to, we have some shared accounts (offline only!) or you can try Minecraft on the Raspberry Pi for free.<br>
+				If you want to code in Minecraft on your own laptop, you might want to try the steps in this <a href="http://wiki.mcrcoderdojo.org.uk/index.php/Minecraft_Python:_Setting_up">installation guide</a> (by Mcr Dojo). If you find any of this tricky, then just bring your laptop along to the dojo and one of the Ninjas or Mentors will help you get set up.</li>
+
+			</ul>
+
 		</div>
 	</div>
-
+</div> <!-- /container -->
 <?php
 include(dirname(__FILE__) . '/../includes/footer.php');
 ?>
