c2e7f0b138de031a3212250e09616f1b2421af1a
Hippyjim <hippyjim@gmail.com>
2015-02-08 17:46:24 +0000

    scratch page has info about scratch and a download link - via Sam

--- webroot/scratch.php
+++ webroot/scratch.php
@@ -5,18 +5,45 @@
  *
  * This file is the "Scratch" page.
  *
+ * Created by Sam from Stockport Dojo - 8th Feb 2015
+ *
  */
 
 /**
  * Include the page header to add the menu at the top of the page.
  */
 include(dirname(__FILE__) . '/../includes/header.php');
+?>
+<div class="container">
+	<div class="row">
+		<h2>Scratch</h2>
+		<div class="col-md-12">
+			<p>Scratch is a simple, visual drag and drop programming language. It's ideal for beginners.</p>
+		</div>
+		<div class="col-md-5 gutter-right">
+			
+			<div class="well">
+				<h3>Download Scratch</h3>
+				<p>You can download scratch from the official Scratch website by following the link below.</p>
+				<p><a class="btn btn-info" href="https://scratch.mit.edu/scratch_1.4/">Download Scratch</a></li></p>
+			</div>
+		</div>
+		<div class="col-md-5 well">
+			<h3>Why Scratch?</h3>
+			<p>Scratch is a program designed to teach people how code used in computer
+programming is structured.</p> <p> It is a flexible platform, often used as a learning
+aid because it lays out the code and the game side by side which helps the user
+think about the code logically as it is run.</p><p> We use scratch for the less
+experienced ninjas although it can be used to create more complicated games
+with a more detailed knowledge of how the code is applied.</p>
+ 
 
-/**
- * Include the placeholder file to fill the page until we have real content
- */
-include(dirname(__FILE__) . '/../includes/placeholder.php');
 
+		</div>
+	</div>
+</div>
+
+<?php
 /**
  * Include the page footer to finish the page neatly.
  */
