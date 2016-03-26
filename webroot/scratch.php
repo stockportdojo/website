4f2adcac1a54df8fb335c85147dedb6b52cc0b80
Hippyjim <hippyjim@gmail.com>
2016-03-26 14:14:07 +0000

    Scratch page includes a link to join scratch online, and a link to a game by Lewis B.

--- webroot/scratch.php
+++ webroot/scratch.php
@@ -27,18 +27,31 @@ include(dirname(__FILE__) . '/../includes/header.php');
 				<p>You can download scratch from the official Scratch website by following the link below.</p>
 				<p><a class="btn btn-info" href="https://scratch.mit.edu/scratch_1.4/">Download Scratch</a></li></p>
 			</div>
+			<div class="well">
+				<h3>Join Scratch</h3>
+				<p>As well as the downloadable version of Scratch there is a web based version that doesn't need installing and allows you to save and share your work if you join it.</p>
+				<p>You can join for free at the link below</p>
+				<p><a class="btn btn-primary" href="https://scratch.mit.edu/">Join Scratch</a></li></p>
+			</div>
 		</div>
-		<div class="col-md-5 well">
-			<h3>Why Scratch?</h3>
-			<p>Scratch is a program designed to teach people how code used in computer
-programming is structured.</p> <p> It is a flexible platform, often used as a learning
-aid because it lays out the code and the game side by side which helps the user
-think about the code logically as it is run.</p><p> We use scratch for the less
-experienced ninjas although it can be used to create more complicated games
-with a more detailed knowledge of how the code is applied.</p>
- 
+		<div class="col-md-5">
+            <div class="well">
+                <h3>Why Scratch?</h3>
+                <p>Scratch is a program designed to teach people how code used in computer
+                    programming is structured.</p> <p> It is a flexible platform, often used as a learning
+                    aid because it lays out the code and the game side by side which helps the user
+                    think about the code logically as it is run.</p><p> We use scratch for the less
+                    experienced ninjas although it can be used to create more complicated games
+                    with a more detailed knowledge of how the code is applied.</p>
 
-
+            </div>
+            <div class="well">
+                <h3>What can you do with Scratch?</h3>
+                <p>Here's an example of the kind of thing you can do with Scratch. This is a game made by one of our Ninjas - Lewis B:</p>
+                <ul>
+                    <li><a href="https://scratch.mit.edu/projects/87090582/">Space Invaders By DerpyBooy2004</a></li>
+                </ul>
+            </div>
 		</div>
 	</div>
 </div>
