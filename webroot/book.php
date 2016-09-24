dc88b84444cddffcf7aa2f044ae876858b7d4e02
jim <hippyjim@gmail.com>
2016-09-24 22:33:25 +0100

    Book page has widget for October 2016. Reskinned and resized - yay!

--- webroot/book.php
+++ webroot/book.php
@@ -26,14 +26,17 @@ include(dirname(__FILE__) . '/../includes/header.php');
 				<h2>Get Me Coding!</h2>
 
 				<p>We run every 3rd Sunday in the month.</p>
-                <p>Our next Dojo is on the 21st August 2016 from 11am to 3pm. Places are limited so please use the form below to book yours. Tickets are available from 1st June 2016.</p>
+                <p>Our next Dojo is on the 16th October 2016 from 11am to 3pm. Places are limited so please use the form below to book yours. Tickets are available from 1st October 2016.</p>
                 <p><em>Don't forget, Ninjas under 12 will need an adult to stay with them.</em></p>
-				<div class="well well-sm">
-					<iframe style="background-color: #FFF;" src="//eventbrite.co.uk/tickets-external?eid=27012992588&ref=etckt" frameborder="0"
-							height="280" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5"
-							scrolling="auto" allowtransparency="true"></iframe>
-
-				</div>
+                <div style="width:100%; text-align:left;">
+                    <iframe
+                        src="//eventbrite.co.uk/tickets-external?eid=28015399815&ref=etckt"
+                        frameborder="0" height="600"
+                        width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5"
+                        scrolling="auto" allowtransparency="true"></iframe>
+                    <div style="font-family:Helvetica, Arial; font-size:12px; padding:10px 0 5px; margin:2px; width:100%; text-align:left;" ><a class="powered-by-eb" style="color: #ADB0B6; text-decoration: none;" target="_blank" href="http://www.eventbrite.co.uk/">Powered by Eventbrite</a>
+                    </div>
+                </div>
 			</div>
 		</div>
 	</div> <!-- /container -->
